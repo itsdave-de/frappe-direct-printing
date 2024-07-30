a125b5ec859cb43dddbc26e7c0fbe6cb2a614fc2
Luiz Gustavo Costa <luizgustavo@luizgustavo.pro.br>
2024-07-30 12:11:37 -0300

    move for another place

--- frappe_direct_printing/frappe_direct_printing/doctype/printer/printer.py
+++ frappe_direct_printing/frappe_direct_printing/doctype/printer/printer.py
@@ -1,19 +1,10 @@
 # Copyright (c) 2024, itsdave GmbH and contributors
 # For license information, please see license.txt
 
-import frappe
+# import frappe
 from frappe.model.document import Document
-import cups
+
 
 class Printer(Document):
 	pass
 
-# Get a list of printers from cups
-@frappe.whitelist()
-def get_printer_list():
-	try:
-		conn = cups.Connection()
-		printers = conn.getPrinters()
-		return [f"{prt} ({printers[prt]['printer-make-and-model'].split(',')[0]})" for prt in printers]
-	except:
-		return ['printer not found']
